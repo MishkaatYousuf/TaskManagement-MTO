c6199999a1557f09c416bdb27849b718c7395d14
MishkaatYousuf <mish9758@gmail.com>
2026-02-04 21:43:58 +0500

    backend into api

---
+++ models/Task.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+const taskSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: String,
+    description: String,
+    status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+
+module.exports = mongoose.model('Task', taskSchema);
